e9f13842f312f7f2361ddecf401d870f49342afa
Richard Mondello <rmondello@gmail.com>
2011-08-28 15:19:51 -0700

    redirect bootstrap to rmondello

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
-git clone git://github.com/codykrieger/dotvim.git ~/.vim
+git clone git://github.com/rmondello/dotvim.git ~/.vim
 cd ~/.vim
 rake install
